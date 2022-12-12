8b9344beb2bc4ed8cf91b63b83ba79a149a15a09
= <=>
2022-12-12 10:23:59 +0700

    third commit

--- app_demo_model/urls.py
+++ app_demo_model/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('upload', views.upload, name='upload'),
-    
+    path('test_predict', views.test_predict, name='test_predict'),
+    path('test', views.test, name="test"),
+
 ]
--- app_demo_model/views.py
+++ app_demo_model/views.py
@@ -4,6 +4,13 @@ from django.http import HttpResponseRedirect
 from django.core.files.storage import FileSystemStorage
 from .models import Grades
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import sklearn.metrics as metrics
 
 
 def upload(request):
@@ -32,3 +39,47 @@ def upload(request):
             obj.save()
             
     return render(request, 'app_demo_model/form.html')
+
+def test_predict(request):
+    return render(request, 'app_demo_model/test_predict.html')
+
+def test(request):
+    #read data
+    data = pd.read_excel(r"D:\pythonTest\data\science-student2.XLSX")
+    
+    #train-test split data
+    X = data.drop("status", axis=1)
+    y = data['status']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    
+    #training model
+    model = DecisionTreeClassifier()
+    model = model.fit(X_train.values, y_train.values)
+    
+    val1 = float(request.POST['r1'])
+    val2 = float(request.POST['r2'])
+    val3 = float(request.POST['r3'])
+    val4 = float(request.POST['r4'])
+    val5 = float(request.POST['r5'])
+    val6 = float(request.POST['r6'])
+    val7 = float(request.POST['r7'])
+    val8 = float(request.POST['r8'])
+    val9 = float(request.POST['r9'])
+    val10 = float(request.POST['r10'])
+    val11 = float(request.POST['r11'])
+    
+    #prediction
+    pred = model.predict([[val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11]])
+    
+    result2 = ""
+    if pred ==[1]:
+        result2 = "True"
+    else:
+        result2 = "FALSE"
+    
+    # print("result2", result2, "\n", "pred", pred)
+    
+        
+    # context = {'result2': result2}
+    
+    return render(request, 'app_demo_model/test_predict.html', {'result2': result2})
