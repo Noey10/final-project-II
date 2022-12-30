2fdcc312618bb9604eb06aa6eb3729910721186a
= <=>
2022-12-30 13:45:06 +0700

    reset password

--- test_project/settings.py
+++ test_project/settings.py
@@ -136,3 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "homepage"
 LOGIN_URL = "login"
+
+#Email
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "test_inbox"
+PASSWORD_RESET_TIMEOUT = 180
