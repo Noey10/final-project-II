193db7bb4cd01786dfd6f2e8631e7976f610c28d
= <=>
2023-02-08 00:16:39 +0700

    update func upload data model

--- app_demo_model/urls.py
+++ app_demo_model/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     #upload model
     path('upload', views.upload_model, name='upload'),
-    path('upload_applied_sci_model', views.upload_applied_sci_model, name='upload_applied_sci_model'),
-    path('upload_health_sci_model', views.upload_health_sci_model, name='upload_health_sci_model'),
-    path('upload_pure_sci_model', views.upload_pure_sci_model, name='upload_pure_sci_model'),
+    path('upload_sci_model', views.upload_sci_model, name='upload_sci_model'),
+    # path('upload_health_sci_model', views.upload_health_sci_model, name='upload_health_sci_model'),
+    # path('upload_pure_sci_model', views.upload_pure_sci_model, name='upload_pure_sci_model'),
     #Applied science model
     path('data_in_applied_sci', views.data_in_applied_sci, name="data_in_applied_sci"),
     path('delete_data_applied', views.delete_data_applied, name="delete_data_applied"),
--- app_demo_model/views.py
+++ app_demo_model/views.py
@@ -19,41 +19,66 @@ def upload_model(request):
     return render(request, 'app_demo_model/upload_model.html')
 
 @login_required
-def upload_applied_sci_model(request):
+def upload_sci_model(request):
     if request.method == 'POST':
         applied = AppliedSciResource()
-        # print(applied)
+        pure = PureSciResource()
+        health = HealthSciResource()
+
         dataset = Dataset()
-        new_applied = request.FILES['appliedfile']
+        new_file = request.FILES['myfile']
         
          #check type file
-        if new_applied.name.endswith('csv'):
-            df = pd.read_csv(new_applied)
-        elif new_applied.name.endswith('xlsx'):
-            df = pd.read_excel(new_applied)
+        if new_file.name.endswith('csv'):
+            df = pd.read_csv(new_file)
+        elif new_file.name.endswith('xlsx'):
+            df = pd.read_excel(new_file)
         else :
             messages.info(request, "ต้องการไฟล์ของข้อมูลที่เป็น excel หรือ csv")
-            return render(request, 'app_demo_model/upload_applied_sci.html')
+            return render(request, 'app_demo_model/upload_model.html')
 
         print('read data')
         df = df.dropna()#delete row missing value
         # print(df.head())
         
+        a = df[(df['major'] == 'ICT')|(df['major'] == 'DSSI')|(df['major'] == 'polymer')]
+        # print(a.head())
+        # print(len(a))
+        
+        h = df[(df['major'] == 'enviSci')|(df['major'] == 'safety')]
+        # print(h.head())
+        # print(len(h))
+        
+        p = df[(df['major'] == 'bio')|(df['major'] == 'chemi')|(df['major'] == 'math')|(df['major'] == 'microBio')|(df['major'] == 'physics')]
+        # print(p.head())
+        # print(len(p))
+        
         #check type column
         for i in df.columns:
             if df.dtypes[i] != np.object_:
                 messages.info(request, "ในคอลัมน์ของเกรดเฉลี่ยต้องการข้อมูล excellent, very good, good, medium, poor, very poor")
-                return render(request, 'app_demo_model/upload_applied_sci.html')
+                return render(request, 'app_demo_model/upload_model.html')
               
-        import_data = dataset.load(df)
+        # import_data = dataset.load(df)
+        import_data = dataset.load(a)
         result = applied.import_data(dataset, dry_run=True, raise_errors=True)
         if not result.has_errors():
-            applied.import_data(dataset, dry_run=False) 
+            applied.import_data(dataset, dry_run=False)
+            
+        import_data1 = dataset.load(h)
+        result2 = health.import_data(dataset, dry_run=True, raise_errors=True)
+        if not result2.has_errors():
+            health.import_data(dataset, dry_run=False) 
+        
+        import_data2 = dataset.load(p)
+        result3 = pure.import_data(dataset, dry_run=True, raise_errors=True)
+        if not result3.has_errors():
+            pure.import_data(dataset, dry_run=False) 
             
         messages.success(request, "อัปโหลดข้อมูลสำเร็จ")
         print('upload success.')
         
-    return render(request, 'app_demo_model/upload_applied_sci.html')
+    return render(request, 'app_demo_model/upload_model.html')
 
 @login_required
 def data_in_applied_sci(request):
@@ -72,36 +97,36 @@ def delete_data_applied(request):
     applied.delete()
     return render(request, 'app_demo_model/data_in_applied_model.html')
 
-@login_required
-def upload_health_sci_model(request):
-    if request.method == 'POST':
-        health = HealthSciResource()
-        dataset = Dataset()
-        new_health = request.FILES['healthfile']
-        print('name file = ', new_health.name)
-        #check type file
-        if not new_health.name.endswith('xlsx'):
-            messages.info(request, "ต้องการไฟล์ของข้อมูลที่เป็น excel")
-            return render(request, 'app_demo_model/upload_health_sci.html')
+# @login_required
+# def upload_health_sci_model(request):
+#     if request.method == 'POST':
+#         health = HealthSciResource()
+#         dataset = Dataset()
+#         new_health = request.FILES['healthfile']
+#         print('name file = ', new_health.name)
+#         #check type file
+#         if not new_health.name.endswith('xlsx'):
+#             messages.info(request, "ต้องการไฟล์ของข้อมูลที่เป็น excel")
+#             return render(request, 'app_demo_model/upload_health_sci.html')
         
-        df = pd.read_excel(new_health)
-        df = df.dropna()#delete row with missing value
+#         df = pd.read_excel(new_health)
+#         df = df.dropna()#delete row with missing value
         
-        #check type column
-        for i in df.columns:
-            if df.dtypes[i] != np.object_:
-                messages.info(request, "ในคอลัมน์ของเกรดเฉลี่ยต้องการข้อมูล excellent, very good, good, medium, poor, very poor")
-                return render(request, 'app_demo_model/upload_health_sci.html')
+#         #check type column
+#         for i in df.columns:
+#             if df.dtypes[i] != np.object_:
+#                 messages.info(request, "ในคอลัมน์ของเกรดเฉลี่ยต้องการข้อมูล excellent, very good, good, medium, poor, very poor")
+#                 return render(request, 'app_demo_model/upload_health_sci.html')
         
-        import_data = dataset.load(df)
-        result = health.import_data(dataset, dry_run=True)
-        if not result.has_errors():
-            health.import_data(dataset, dry_run=False)       
-        messages.success(request, "อัปโหลดข้อมูลสำเร็จ")
+#         import_data = dataset.load(df)
+#         result = health.import_data(dataset, dry_run=True)
+#         if not result.has_errors():
+#             health.import_data(dataset, dry_run=False)       
+#         messages.success(request, "อัปโหลดข้อมูลสำเร็จ")
         
-        print('upload success.')
+#         print('upload success.')
         
-    return render(request, 'app_demo_model/upload_health_sci.html')
+#     return render(request, 'app_demo_model/upload_health_sci.html')
 
 @login_required
 def data_in_health_sci(request):
@@ -119,35 +144,35 @@ def delete_data_health(request):
     applied.delete()
     return render(request, 'app_demo_model/data_in_health_model.html')
 
-@login_required
-def upload_pure_sci_model(request):
-    if request.method == 'POST':
-        pure = PureSciResource()
-        dataset = Dataset()
-        new_pure = request.FILES['purefile']
-        print('name file = ', new_pure.name)
+# @login_required
+# def upload_pure_sci_model(request):
+#     if request.method == 'POST':
+#         pure = PureSciResource()
+#         dataset = Dataset()
+#         new_pure = request.FILES['purefile']
+#         print('name file = ', new_pure.name)
         
-        if not new_pure.name.endswith('xlsx'):
-            messages.info(request, "ต้องการไฟล์ของข้อมูลที่เป็น excel")
-            return render(request, 'app_demo_model/upload_pure_sci.html')
+#         if not new_pure.name.endswith('xlsx'):
+#             messages.info(request, "ต้องการไฟล์ของข้อมูลที่เป็น excel")
+#             return render(request, 'app_demo_model/upload_pure_sci.html')
         
-        df = pd.read_excel(new_pure)
-        df = df.dropna()#delete row with missing value
+#         df = pd.read_excel(new_pure)
+#         df = df.dropna()#delete row with missing value
 
-        #check type column
-        for i in df.columns:
-            if df.dtypes[i] != np.object_:
-                messages.info(request, "ในคอลัมน์ของเกรดเฉลี่ยต้องการข้อมูล excellent, very good, good, medium, poor, very poor")
-                return render(request, 'app_demo_model/upload_pure_sci.html')
+#         #check type column
+#         for i in df.columns:
+#             if df.dtypes[i] != np.object_:
+#                 messages.info(request, "ในคอลัมน์ของเกรดเฉลี่ยต้องการข้อมูล excellent, very good, good, medium, poor, very poor")
+#                 return render(request, 'app_demo_model/upload_pure_sci.html')
         
-        import_data = dataset.load(df)
-        result = pure.import_data(dataset, dry_run=True)
-        if not result.has_errors():
-            pure.import_data(dataset, dry_run=False)
+#         import_data = dataset.load(df)
+#         result = pure.import_data(dataset, dry_run=True)
+#         if not result.has_errors():
+#             pure.import_data(dataset, dry_run=False)
         
-        messages.success(request, "อัปโหลดข้อมูลสำเร็จ")
+#         messages.success(request, "อัปโหลดข้อมูลสำเร็จ")
         
-    return render(request, 'app_demo_model/upload_pure_sci.html')
+#     return render(request, 'app_demo_model/upload_pure_sci.html')
 
 @login_required
 def data_in_pure_sci(request):
